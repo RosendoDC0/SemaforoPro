1cfc680ee1b46e922f52a81dc3484d593b3259bf
RosendoDC <rosendodc02@gmail.com>
2026-05-03 21:56:27 -0500

    deploy config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,12 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
 
+  ssr: false,
+
+  nitro: {
+    preset: 'github-pages'
+  },
+
   build: {
     transpile: ['vuetify'],
   },
